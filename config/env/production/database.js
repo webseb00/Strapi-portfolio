392e4e82f7bbe34ecd0f54a081d6e8f8cabc2c44
webseb00 <lordow91@gmail.com>
2022-11-23 20:20:41 +0100

    Initialize repo

---
+++ config/env/production/database.js
@@ -0,0 +1,19 @@
+import { parse } from 'pg-connection-string'
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password
+      },
+      debug: false,
+    },
+  };
+};
